07f6dc55464954b95515d78d86daf80151b63755
Walysson Rodrigues de Oliveira <walysson@aladim.com.br>
2026-05-27 18:00:45 -0300

    Fix typescript error in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,7 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon.svg',
         sizes: 'any',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
     ],
   };
